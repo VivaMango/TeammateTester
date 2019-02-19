da1a14e56220c3d79cd7c266403ebc9392d4dd11
VivaMango <VivaMangoMail@gmail.com>
2019-02-19 08:46:12 -0500

    headed to office Tues AM

--- app/data/teammates.js
+++ app/data/teammates.js
@@ -1,1 +1,5 @@
 console.log("TEAMMATESJS TEST")
+
+$("#surveySubmit").on("click" , function() {
+    event.preventDefault();
+})
--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 var express = require("express");
+var app = express();
 var path = require("path");
+var PORT = 4000
 
 console.log("SERVERJS TEST")
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("app"))
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
+
+app.listen(PORT, function() {
+    console.log(`APP NOW LISTENING ON ${PORT}`);
+});
