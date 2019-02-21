98a2c22eb607be9d2bb4a91b1787cc0c3acc196b
VivaMango <VivaMangoMail@gmail.com>
2019-02-21 16:12:01 -0500

    home button added, links back and forth working, getting ready to separate routes from server.js

--- app/public/js/client.js
+++ app/public/js/client.js
@@ -1,5 +1,11 @@
 console.log("CLIENTJS LINKED")
 
-$("#surveyLink").on("click" , function () {
-    // event.preventDefault()
+$("#surveySubmit").on("click" , function () {
+    event.preventDefault();
+    var answerArray = []
+    $(".custom-select").each(function(i) {
+        var answerVal = ($(this).val())
+        answerArray.push(answerVal)
+    })
 })
+
